9c6cb33ba41c74089fc8c2ec8d8a71684c7dfbcc
Mikhail Gerasimov <mgerasim@inbox.ru>
2015-06-02 13:41:19 +1100

    init

---
+++ GeospaceEntity/Models/Station.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeospaceEntity.Models
+{
+    public class Station
+    {
+        int ID;
+        int Code;
+        DateTime created_at { get; set; }
+        DateTime updated_at { get; set; }
+    }
+}
