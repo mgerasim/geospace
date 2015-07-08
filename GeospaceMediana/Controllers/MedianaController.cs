13e5ec7358ed5790b15d4747afe573fef67edffd
Tiikara <tiikara93@gmail.com>
2015-07-08 11:45:01 +1000

    Вывод медианы для f0F2 и M3000f2

--- GeospaceMediana/Controllers/MedianaController.cs
+++ GeospaceMediana/Controllers/MedianaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GeospaceMediana.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,10 +12,37 @@ namespace GeospaceMediana.Controllers
         //
         // GET: /Mediana/
 
-        public ActionResult Index()
+        public ActionResult Index(int year = -1, int month = -1, int station = 43501, string type = "f0F2")
         {
             @ViewBag.Title = "Медиана";
-            return View();
+
+            DateTime nowDateTime = DateTime.Now;
+
+            if(month == -1)
+            {
+                month = nowDateTime.Month;
+            }
+
+            if(year == -1)
+            {
+                year = nowDateTime.Year;
+            }
+
+            DateTime startMonth = new DateTime(year, month, 1);
+
+            string start = String.Format("{0:yyyyMMdd}", startMonth);
+            int countDays = DateTime.DaysInMonth(startMonth.Year, startMonth.Month);
+            ViewIonka Model = new ViewIonka(station, start, countDays, countDays);
+
+            ViewBag.Date = startMonth.ToString("MMMM yyyy", System.Globalization.CultureInfo.CurrentCulture);
+            ViewBag.CountDaysInMonth = countDays;
+
+            ViewBag.Month = startMonth.Month;
+            ViewBag.Year = startMonth.Year;
+
+            ViewBag.Type = type;
+
+            return View(Model);
         }
 
     }
