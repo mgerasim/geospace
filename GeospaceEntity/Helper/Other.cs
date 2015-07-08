521472251b899a2482b335175c0fc47638d7134e
distomin <i.d.p@bk.ru>
2015-07-08 04:14:35 +0000

    Вывод всех станций

---
+++ GeospaceEntity/Helper/Other.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeospaceEntity.Models;
+using System.IO;
+
+namespace GeospaceEntity.Helper
+{
+    public static class Other
+    {
+        //печатает индексы всех станций
+        public static void Print_All_Stations( int statIndex, List<int> listIndex, string pathFile)
+        {
+            foreach (int index in listIndex)
+            {
+                if (index == statIndex) return;
+            }
+
+            listIndex.Add(statIndex);
+
+            StreamWriter sw = new StreamWriter(pathFile, true);
+            sw.WriteLine(statIndex);
+            sw.Close();
+        }
+    }
+}
