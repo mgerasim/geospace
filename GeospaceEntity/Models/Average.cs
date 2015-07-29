6abd19b36cf0395a87fd19b3e7b1942a0133d80d
Mikhail Gerasimov <mgerasim@inbox.ru>
2015-07-29 06:47:15 +0000

    average

---
+++ GeospaceEntity/Models/Average.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GeospaceEntity.Models
+{
+    public class Average
+    {
+        public virtual Station Station { get; set; }
+        public virtual int ID { get; set; }
+        public virtual DateTime created_at { get; set; }
+        public virtual DateTime updated_at { get; set; }
+        public virtual int HH { get; set; }
+        public virtual int MM { get; set; }
+        public virtual int DD { get; set; }
+        public virtual int YYYY { get; set; }
+        public virtual int MI { get; set; }
+       
+        public virtual int F2_05 { get; set; }
+        public virtual int F2_07 { get; set; }
+        public virtual int F2_10 { get; set; }
+        public virtual int F2_20 { get; set; }
+        public virtual int F2_27 { get; set; }
+        public virtual int F2_30 { get; set; }
+        public virtual int M3000_05 { get; set; }
+        public virtual int M3000_07 { get; set; }
+        public virtual int M3000_10 { get; set; }
+        public virtual int M3000_20 { get; set; }
+        public virtual int M3000_27 { get; set; }
+        public virtual int M3000_30 { get; set; }
+    
+
+
+        public Average()
+        {
+            
+        }
+
+       
+    }
+}
---
+++ GeospaceEntity/Repositories/AverageRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeospaceEntity.Common;
+using NHibernate;
+using NHibernate.Criterion;
+
+namespace GeospaceEntity.Repositories
+{
+    public class AverageRepository : IRepository<GeospaceEntity.Models.Average>
+    {
+        #region IRepository<Average> Members
+
+        void IRepository<GeospaceEntity.Models.Average>.Save(GeospaceEntity.Models.Average entity)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Save(entity);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        void IRepository<GeospaceEntity.Models.Average>.Update(GeospaceEntity.Models.Average entity)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Update(entity);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        void IRepository<GeospaceEntity.Models.Average>.Delete(GeospaceEntity.Models.Average entity)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Delete(entity);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        GeospaceEntity.Models.Average IRepository<GeospaceEntity.Models.Average>.GetById(int id)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+                return session.CreateCriteria<GeospaceEntity.Models.Average>().Add(Restrictions.Eq("ID", id)).UniqueResult<GeospaceEntity.Models.Average>();
+        }
+
+       
+        IList<GeospaceEntity.Models.Average> IRepository<GeospaceEntity.Models.Average>.GetAll()
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                ICriteria criteria = session.CreateCriteria(typeof(GeospaceEntity.Models.Average));
+                criteria.AddOrder(Order.Asc("ID"));
+                return criteria.List<GeospaceEntity.Models.Average>();
+            }
+        }
+
+        #endregion
+    }
+}
