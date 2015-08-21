33be2a81b17bddb67697f4825dac82991faaf01c
Mikhail Gerasimov <mgerasim@inbox.ru>
2015-08-21 02:50:38 +0000

    add-disturbance

---
+++ GeospaceEntity/Models/Disturbance.cs
@@ -0,0 +1,50 @@
+﻿using GeospaceEntity.Common;
+using GeospaceEntity.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeospaceEntity.Models
+{
+    public class Disturbance
+    {
+        public virtual int ID { get; set; }
+        public virtual DateTime created_at {get; set;}
+        public virtual DateTime updated_at {get; set;}
+
+        public virtual Station Station { get; set; }
+        public virtual int YYYY { get; set; }
+        public virtual int MM { get; set; }
+        public virtual int DD { get; set; }
+        public virtual int HH { get; set; }
+        
+
+        public Disturbance()
+        {
+
+        }
+
+        public virtual void Save()
+        {
+            IRepository<Disturbance> repo = new DisturbanceRepository();
+            this.created_at = DateTime.Now;
+            this.updated_at = DateTime.Now;
+            repo.Save(this);
+        }
+
+        public virtual void Update()
+        {
+            IRepository<Disturbance> repo = new DisturbanceRepository();
+            this.updated_at = DateTime.Now;
+            repo.Update(this);
+        }
+
+        public virtual List<Disturbance> GetAll()
+        {
+            IRepository<Disturbance> repo = new DisturbanceRepository();
+            repo.GetAll();
+            return (List<Disturbance>)repo.GetAll();
+        }
+    }
+}
---
+++ GeospaceEntity/Repositories/DisturbanceRepository.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeospaceEntity.Common;
+using NHibernate;
+using NHibernate.Criterion;
+using GeospaceEntity.Models;
+
+namespace GeospaceEntity.Repositories
+{
+    public class DisturbanceRepository : IRepository<GeospaceEntity.Models.Disturbance>
+    {
+        #region IRepository<Disturbance> Members
+
+        void IRepository<GeospaceEntity.Models.Disturbance>.Save(GeospaceEntity.Models.Disturbance entity)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Save(entity);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        void IRepository<GeospaceEntity.Models.Disturbance>.Update(GeospaceEntity.Models.Disturbance entity)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Update(entity);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        void IRepository<GeospaceEntity.Models.Disturbance>.Delete(GeospaceEntity.Models.Disturbance entity)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Delete(entity);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        GeospaceEntity.Models.Disturbance IRepository<GeospaceEntity.Models.Disturbance>.GetById(int id)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+                return session.CreateCriteria<GeospaceEntity.Models.Disturbance>().Add(Restrictions.Eq("ID", id)).UniqueResult<GeospaceEntity.Models.Disturbance>();
+        }
+
+        public IList<GeospaceEntity.Models.Disturbance> GetByRangeNumber(Station station, int YYYY, int MM, int rangeNumber)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+                return session.CreateCriteria<GeospaceEntity.Models.Disturbance>()
+                        .Add(Restrictions.Eq("Station", station))
+                        .Add(Restrictions.Eq("YYYY", YYYY))
+                        .Add(Restrictions.Eq("MM", MM))
+                        .Add(Restrictions.Eq("RangeNumber", rangeNumber))
+                        .List<GeospaceEntity.Models.Disturbance>();
+        }
+
+        public IList<GeospaceEntity.Models.Disturbance> GetByMonth(Station station, int YYYY, int MM)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+                return session.CreateCriteria<GeospaceEntity.Models.Disturbance>()
+                        .Add(Restrictions.Eq("Station", station))
+                        .Add(Restrictions.Eq("YYYY", YYYY))
+                        .Add(Restrictions.Eq("MM", MM))
+                        .List<GeospaceEntity.Models.Disturbance>();
+        }
+
+        public GeospaceEntity.Models.Disturbance GetByDate(Station station, int YYYY, int MM, int HH, int rangeNumber)
+        {
+            GeospaceEntity.Models.Disturbance Disturbance = null;
+            
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                try
+                {
+                    Disturbance = session.CreateCriteria<GeospaceEntity.Models.Disturbance>()
+                        .Add(Restrictions.Eq("Station", station))
+                        .Add(Restrictions.Eq("YYYY", YYYY))
+                        .Add(Restrictions.Eq("MM", MM))
+                        .Add(Restrictions.Eq("HH", HH))
+                        .Add(Restrictions.Eq("RangeNumber", rangeNumber))
+                        .UniqueResult<GeospaceEntity.Models.Disturbance>();
+                }
+                catch(Exception)
+                {
+                    Disturbance = null;
+                }
+            }
+
+            if (Disturbance == null)
+                Disturbance = new Disturbance();
+
+            return Disturbance;
+        }
+
+        IList<GeospaceEntity.Models.Disturbance> IRepository<GeospaceEntity.Models.Disturbance>.GetAll()
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                ICriteria criteria = session.CreateCriteria(typeof(GeospaceEntity.Models.Station));
+                return criteria.List<GeospaceEntity.Models.Disturbance>();
+            }
+        }
+
+        #endregion
+    }
+}
