d7a7f6f4d335b6d0888e1b94b66d7eca2f4e356e
Mikhail Gerasimov <mgerasim@inbox.ru>
2015-09-16 04:15:23 +0000

    fix-umagf-edit-navigate

--- GeospaceMediana/Scripts/umagf-edit.js
+++ GeospaceMediana/Scripts/umagf-edit.js
@@ -1,5 +1,182 @@
 ﻿(function () {
-    
+    function getCell(day, type) {
+        return $('td')
+                .filter('[data-day="' + day + '"]')
+                .filter('[data-type="' + type + '"]');
+    }
+
+    function moveEdit(currentCell, keyCode) {
+        var day = currentCell.data("day");
+        var type = currentCell.data("type");
+
+        if (type == "ak" && (keyCode == 39 || keyCode == 40)) {
+            var newCell = getCell(day, "k1");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+
+        if (type == "k1" && keyCode == 39) {
+            var newCell = getCell(day, "k2");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+
+        if (type == "k2" && keyCode == 39) {
+            var newCell = getCell(day, "k3");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+
+
+        if (type == "k3" && keyCode == 39) {
+            var newCell = getCell(day, "k4");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+
+        if (type == "k4" && keyCode == 39) {
+            var newCell = getCell(day, "k5");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+
+        if (type == "k5" && keyCode == 39) {
+            var newCell = getCell(day, "k6");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+        
+
+        if (type == "k6" && keyCode == 39) {
+            var newCell = getCell(day, "k7");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+        
+
+        if (type == "k7" && keyCode == 39) {
+            var newCell = getCell(day, "k8");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+
+
+        if (type == "k8" && keyCode == 39) {
+            var newCell = getCell(day, "events");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+
+        if((type == "k1" ||
+            type == "k2" ||
+            type == "k3" ||
+            type == "k4" ||
+            type == "k5" ||
+            type == "k6" ||
+            type == "k7" ||
+            type == "k8") && keyCode == 40)
+        {
+            var newCell = getCell(day, "events");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+
+
+        //
+        if (type == "events" && (keyCode == 37 || keyCode == 38)) {
+            var newCell = getCell(day, "k8");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+
+        if (type == "k8" && keyCode == 37) {
+            var newCell = getCell(day, "k7");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+
+        if (type == "k7" && keyCode == 37) {
+            var newCell = getCell(day, "k6");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+
+
+        if (type == "k6" && keyCode == 37) {
+            var newCell = getCell(day, "k5");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+
+        if (type == "k5" && keyCode == 37) {
+            var newCell = getCell(day, "k4");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+
+        if (type == "k4" && keyCode == 37) {
+            var newCell = getCell(day, "k3");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+
+
+        if (type == "k3" && keyCode == 37) {
+            var newCell = getCell(day, "k2");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+
+
+        if (type == "k2" && keyCode == 37) {
+            var newCell = getCell(day, "k1");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+
+
+        if (type == "k1" && keyCode == 37) {
+            var newCell = getCell(day, "ak");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+
+        if ((type == "k1" ||
+            type == "k2" ||
+            type == "k3" ||
+            type == "k4" ||
+            type == "k5" ||
+            type == "k6" ||
+            type == "k7" ||
+            type == "k8") && keyCode == 38) {
+            var newCell = getCell(day, "ak");
+            $('#edit').blur();
+            newCell.dblclick();
+            return;
+        }
+
+        $('#edit').blur();
+    }
+
     function editEvent(e) {
         var currentCell = $(this);
         var id = currentCell.data("id");
@@ -33,6 +210,14 @@
         $('#edit').width(cellWidth - 2); // 1+1 border width
         $('#edit').height(cellHeight - 1);
         currentCell.width(cellWidth);
+
+        $('#edit').keydown(function (event) {
+            if (event.keyCode >= 37 && event.keyCode <= 40) {
+                event.preventDefault();
+                    
+                moveEdit(currentCell, event.keyCode);
+            }
+        });
 
         $('#edit').focus();
         $('#edit').select();
